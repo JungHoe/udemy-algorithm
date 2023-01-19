b483720d27c6da9e1238dbf8869c5431e41f72fb
JungHoe <junghwi0603@gmail.com>
2023-01-19 16:47:55 +0900

    제귀문제풀기(쉬움)

---
+++ src/recursiveQuest_easy.js
@@ -0,0 +1,63 @@
+"use strict";
+// power 재귀 구현
+function power(number, times) {
+  return times === 0 ? 1 : number * power(number, times - 1);
+}
+// console.log(power(2, 0)); // 1
+// console.log(power(2, 2)); // 4
+// console.log(power(2, 4)); // 16
+// Factorial 재귀구현
+function factorial(num) {
+  if (num < 0) {
+    throw new Error("Not allowed number");
+  }
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+// console.log(factorial(2));
+// console.log(factorial(3));
+// console.log(factorial(4));
+// console.log(factorial(5));
+function productOfArray(arr) {
+  function helper(idx) {
+    if (idx === 0) {
+      return arr[idx];
+    }
+    return arr[idx] * helper(idx - 1);
+  }
+  const result = helper(arr.length - 1);
+  return result;
+}
+// productOfArray([1, 2, 3]); // 6
+// productOfArray([1, 2, 3, 10]); // 60
+function recursiveRange(num) {
+  if (num === 0) {
+    return 0;
+  } else {
+    return num + recursiveRange(num - 1);
+  }
+}
+recursiveRange(6); // 21
+recursiveRange(10); // 55
+// 피보나치숫자 찾기
+function fib(nTH) {
+  if (nTH < 1) {
+    throw new Error("Not Allowed Value");
+  }
+  if (nTH === 1 || nTH === 2) {
+    return 1;
+  }
+  return fib(nTH - 1) + fib(nTH - 2);
+}
+// 1 1 2 3 5 8 13
+console.log(fib(1));
+console.log(fib(2));
+console.log(fib(3));
+console.log(fib(4));
+console.log(fib(5));
+console.log(fib(6));
+console.log(fib(7));
+// fib(4); // 3;
+//# sourceMappingURL=run.js.map
