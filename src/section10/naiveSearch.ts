571a3cb570debdf3534449ae9f9fc1560b356fc7
JungHoe <junghwi0603@gmail.com>
2023-01-30 15:01:38 +0900

    section10 naive search

---
+++ src/section10/naiveSearch.ts
@@ -0,0 +1,23 @@
+function naiveSearch(long: string, short: string): number {
+  let result = 0;
+  const LoopIndex = long.length - short.length + 1;
+  for (let i = 0; i < LoopIndex; i += 1) {
+    for (let j = 0; j < short.length; j += 1) {
+      const shortChar = short[j];
+      if (long[i + j] === shortChar) {
+        if (j === short.length - 1) {
+          result += 1;
+        }
+      } else {
+        break;
+      }
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+// naiveSearch("omgekwl", "zz"); // 0
+// naiveSearch("omgekwl", "mg"); // 1
+// naiveSearch("ooo", "oo"); // 2
+// naiveSearch("AAAAAAA", "AAA"); // 5
